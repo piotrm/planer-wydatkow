206fbcd506b70764d4f9ee6ad93916fa3115ec95
Piotrek Matuszkiewicz <piotrek@applicake.com>
2013-02-16 10:21:18 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Planer::Application.config.secret_token = 'a5a841380043bd8ade68330804686fba5a1675ccd34b6970a546c606b20ff7bb5860b386fce26a42f67149ab5ea2ed1d661176a7901fa157c089049ea6ce81de'
